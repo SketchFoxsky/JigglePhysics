7618a7b414756ae7915b1694e3c51d93287988a1
naelstrof <naelstrof@gmail.com>
2026-02-09 21:37:56 -0700

    fixed typo

--- Assets/JigglePhysicsExample/ThirdParty/Scripts/SimpleCameraController.cs
+++ Assets/JigglePhysicsExample/ThirdParty/Scripts/SimpleCameraController.cs
@@ -1,4 +1,5 @@
 ﻿using UnityEngine;
+using UnityEngine.InputSystem;
 
 namespace UnityTemplateProjects {
     public class SimpleCameraController : MonoBehaviour {
@@ -10,15 +11,18 @@ namespace UnityTemplateProjects {
         public float distance = 10f;
         private Vector2 offset;
         void Simulate() {
-            if (Input.GetButton("Fire1") || Input.GetButton("Fire2")) {
-                offset += new Vector2(Input.GetAxis("Mouse X"), -Input.GetAxis("Mouse Y"))*2f;
+            if (Mouse.current.leftButton.isPressed || Mouse.current.rightButton.isPressed) {
+                var delta = Mouse.current.delta.ReadValue();
+                delta.y *= -1;
+                offset += delta*0.1f;
             } else {
                 //offset += new Vector2(Input.GetAxis("Horizontal"), 0f)*2f;
             }
             transform.rotation = Quaternion.AngleAxis(offset.x,Vector3.up)*Quaternion.AngleAxis(offset.y,Vector3.right);
             transform.position = Vector3.SmoothDamp(transform.position, targetLookat.position - transform.forward*distance, ref vel, 0.04f);
             transform.rotation = Quaternion.LookRotation((targetLookat.position - transform.position).normalized, Vector3.up);
-            distance -= Input.GetAxis("Mouse ScrollWheel");
+            var scroll = Mouse.current.scroll.ReadValue();
+            distance -= scroll.y;
             distance = Mathf.Max(distance, 1f);
         }
         public void LateUpdate() {
